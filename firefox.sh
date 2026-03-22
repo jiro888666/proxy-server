71520255c028eccd1aa7efd553810feba3935515
jiro888666 <jiro12345@qq.com>
2026-03-22 13:33:52 +0800

    Delete firefox.sh

--- firefox.sh
+++
@@ -1,9 +0,0 @@
-docker rm -f firefox
-docker run -d \
---name=firefox \
--p 5800:5800 \
--v $(pwd)/firefox1:/config:rw \
--e LANG='zh.UTF-8' \
--e VNC_PASSWORD='firefox1234' \
--e TZ=Asia/Shanghai \
-registry.cn-hangzhou.aliyuncs.com/jiro-jlzhang/common:jlesage-firefox-v25.04.1.font
